57f67091ee5cbfd4c8d8869af36685b2c2fa30c8
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2021-04-06 15:50:21 +0700

    Create a Lists component to group our lists

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import { listLists } from './graphql/queries';
 import 'Semantic-UI-CSS/semantic.min.css';
 import React, { useEffect, useState } from 'react';
 import MainHeader from './components/headers/MainHeader';
+import Lists from './components/List/Lists';
 
 Amplify.configure(awsConfig);
 
 function App() {
-  const [list, setList] = useState([]);
+  const [lists, setLists] = useState([]);
 
   async function fetchList() {
     const { data } = await API.graphql(graphqlOperation(listLists));
-    setList(data.listLists.items);
+    setLists(data.listLists.items);
     console.log(data);
   }
 
@@ -26,11 +27,7 @@ function App() {
       <AmplifySignOut />
       <div className="App">
         <MainHeader />
-        <ul>
-          {list.map((item) => (
-            <li key={item.id}>{item.title}</li>
-          ))}
-        </ul>
+        <Lists lists={lists} />
       </div>
     </AmplifyAuthenticator>
   );
