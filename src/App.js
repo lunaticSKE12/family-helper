3ccfe6842ea3aa433633820aa79de52dc3e3ab6f
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2021-04-10 15:53:20 +0700

    Cleaning up the subscription in useEffect

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { listLists } from './graphql/queries';
 import { createList } from './graphql/mutations';
 import { onCreateList } from './graphql/subscriptions';
 import 'semantic-ui-css/semantic.min.css';
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import MainHeader from './components/headers/MainHeader';
 import Lists from './components/List/Lists';
 import { Button, Container, Form, Icon, Modal } from 'semantic-ui-react';
@@ -16,6 +16,7 @@ Amplify.configure(awsConfig);
 const initialState = {
   title: '',
   description: '',
+  lists: [],
   isModalOpen: false,
 };
 
@@ -25,6 +26,8 @@ const listReducer = (state = initialState, action) => {
       return { ...state, description: action.value };
     case 'TITLE_CHANGED':
       return { ...state, title: action.value };
+    case 'UPDATE_LISTS':
+      return { ...state, lists: [...action.value, ...state.lists] };
     case 'OPEN_MODAL':
       return { ...state, isModalOpen: true };
     case 'CLOSE_MODAL':
@@ -38,12 +41,9 @@ const listReducer = (state = initialState, action) => {
 function App() {
   const [state, dispatch] = useReducer(listReducer, initialState);
 
-  const [lists, setLists] = useState([]);
-  const [newList, setNewList] = useState('');
-
   const fetchList = async () => {
     const { data } = await API.graphql(graphqlOperation(listLists));
-    setLists(data.listLists.items);
+    dispatch({ type: 'UPDATE_LISTS', value: data.listLists.items });
   };
 
   useEffect(() => {
@@ -51,18 +51,13 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (newList !== '') {
-      setLists([newList, ...lists]);
-    }
-  }, [newList]);
-
-  const addToList = ({ data }) => {
-    setNewList(data.onCreateList);
-  };
-  useEffect(() => {
     let subscription = API.graphql(graphqlOperation(onCreateList)).subscribe({
-      next: ({ provider, value }) => addToList(value),
+      next: ({ provider, value }) => {
+        console.log(value);
+        dispatch({ type: 'UPDATE_LISTS', value: [value.data.onCreateList] });
+      },
     });
+    return () => subscription.unsubcribe();
   }, []);
 
   const saveList = async () => {
@@ -85,7 +80,7 @@ function App() {
         </Button>
         <div className="App">
           <MainHeader />
-          <Lists lists={lists} />
+          <Lists lists={state.lists} />
         </div>
       </Container>
       <Modal open={state.isModalOpen} dimmer="blurring">
